3243cd8a17c3a2a60d2aeda1df3a2b2852bfbe17
dhp551 <dhp551@gmail.com>
2014-08-01 11:02:21 +0530

    First Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'dfbb056d8ea52e828d361c7fb25546038d71e5e56360ad50d7606f9c45e7cb99135a8e364db2d449ab8b50c423a3ec851d12f05b0fd5523de9da1d45dc1c8ead'
